aaa1efca888cc0b1fd2c0dc5653663733f62a0e5
mpeterson114 <mlpeterson114@gmail.com>
2024-04-11 11:28:14 +0200

    more git config tests

--- cpp01/ex01/src/main.cpp
+++ cpp01/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mpeterso <mpeterso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 09:44:47 by mpeterso          #+#    #+#             */
-/*   Updated: 2024/04/11 11:24:50 by mpeterso         ###   ########.fr       */
+/*   Updated: 2024/04/11 11:27:56 by mpeterso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,7 @@ int main()
     int i = 0;
     int N = 7;
 
-    Zombie *zombie = zombieHorde(N, "zOmBiEeeee");
-    
+    Zombie *zombie = zombieHorde(N, "zOmBiEeeee"); 
 	while (i < N){
         zombie[i].announce();
         i++;
