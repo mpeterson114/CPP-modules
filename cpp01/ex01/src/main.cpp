bf8970c3e1f083ee361532c4d7551918aeaf7ad8
mpeterson114 <mlpeterson114@gmail.com>
2024-04-11 11:25:02 +0200

    commit testing

--- cpp01/ex01/src/main.cpp
+++ cpp01/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mpeterso <mpeterso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 09:44:47 by mpeterso          #+#    #+#             */
-/*   Updated: 2024/04/02 12:52:36 by mpeterso         ###   ########.fr       */
+/*   Updated: 2024/04/11 11:24:50 by mpeterso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ int main()
     int N = 7;
 
     Zombie *zombie = zombieHorde(N, "zOmBiEeeee");
-    while (i < N){
+    
+	while (i < N){
         zombie[i].announce();
         i++;
     }
