260d2ce4513c08423aafbcb84bdb5b1cbb2e9d5d
Megan Peterson <mlpeterson114@gmail.com>
2024-09-12 19:26:03 +0200

    05/00

---
+++ cpp05/ex00/src/Bureaucrat.cpp
@@ -0,0 +1,76 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat()
+{
+    std::cout << "Bureaucrat created with default constructor" << std::endl;
+}
+        
+Bureaucrat::Bureaucrat(std::string const &name, int grade) : _name(name)
+{
+    if (grade > 150)
+        throw Bureaucrat::GradeTooLowException();
+    else if (grade < 1)
+        throw Bureaucrat::GradeTooHighException();
+    this->_grade = grade;
+    std::cout << "\033[32mBureaucrat " << this->_name << " has been created\033[0m" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(Bureaucrat const &copy) : _name(copy._name), _grade(copy._grade)
+{
+    std::cout << "Copy constructor called" << std::endl;
+}
+
+Bureaucrat &Bureaucrat::operator=(Bureaucrat const &copy)
+{
+    std::cout << "Assignment operator called" << std::endl;
+    if (this != &copy)
+    {
+        this->_grade = copy._grade;
+    }
+    return *this;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+    std::cout << "\033[38;5;161mBureaucrat " << this->_name << " has been destroyed\033[0m" << std::endl;
+}
+
+std::string const &Bureaucrat::getName() const
+{
+    return this->_name;
+}
+
+int const &Bureaucrat::getGrade() const
+{
+    return this->_grade;
+}
+
+void Bureaucrat::increment(int amount)
+{
+    if (this->_grade - amount < 1)
+        throw Bureaucrat::GradeTooHighException();
+    this->_grade -= amount;
+}
+
+void Bureaucrat::decrement(int amount)
+{
+    if (this->_grade + amount > 150)
+        throw Bureaucrat::GradeTooLowException();
+    this->_grade += amount;
+}
+
+std::ostream& operator<<(std::ostream& stream, const Bureaucrat& other)
+{
+    stream << other.getName() << ", bureaucrat grade " << other.getGrade();
+    return (stream);
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+    return "\033[38;5;209mGrade too high! Max value allowed = 1\033[0m";
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+    return "\033[38;5;209mGrade too low! Min value allowed = 150\033[0m";
+}
