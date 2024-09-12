c13413f24967a1392406ec7743d1f75f58804364
Megan Peterson <mlpeterson114@gmail.com>
2024-09-12 19:30:08 +0200

    05/01 started

---
+++ cpp05/ex01/inc/Bureaucrat.hpp
@@ -0,0 +1,41 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <string>
+#include <exception>
+
+class Bureaucrat
+{
+    private:
+        std::string const _name;
+        int _grade;
+
+    public:
+        Bureaucrat();
+        Bureaucrat(std::string const &name, int grade);
+        Bureaucrat(Bureaucrat const &copy);
+        Bureaucrat &operator=(Bureaucrat const &copy);
+        ~Bureaucrat();
+
+        std::string const &getName() const;
+        int const &getGrade() const;
+        void increment(int amount);
+        void decrement(int amount);
+
+        class GradeTooLowException : public std::exception
+        {
+            public:
+                virtual const char *what() const throw();
+        };
+        class GradeTooHighException : public std::exception
+        {
+            public:
+                virtual const char *what() const throw();
+        };
+};
+
+std::ostream& operator<<(std::ostream& stream, const Bureaucrat& other);
+
+
+#endif
---
+++ cpp05/ex01/src/main.cpp
@@ -0,0 +1,65 @@
+#include "Bureaucrat.hpp"
+
+int main()
+{
+    Bureaucrat A("A", 10);
+    std::cout << "-Name: " << A.getName() << std::endl;
+    std::cout << "-Grade: " << A.getGrade() << std::endl;
+    std::cout << "\033[34m   Incrementing...\033[0m" << std::endl;
+    A.increment(8);
+    std::cout << "New grade: " << A.getGrade() << std::endl;
+    try
+    {
+        while (A.getGrade() >= 1)
+        {
+            std::cout << "\033[34m   Incrementing...\033[0m" << std::endl;
+            A.increment(1);
+            std::cout << "New grade: " << A.getGrade() << std::endl;
+        }
+    }
+    catch (const std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    std::cout << std::endl;
+    Bureaucrat B("B", 140);
+    std::cout << "-Name: " << B.getName() << std::endl;
+    std::cout << "-Grade: " << B.getGrade() << std::endl;
+    std::cout << "\033[34m   Decrementing...\033[0m" << std::endl;
+    B.decrement(8);
+    std::cout << "New grade: " << B.getGrade() << std::endl;
+    try
+    {
+        while (B.getGrade() <= 150)
+        {
+            std::cout << "\033[34m   Decrementing...\033[0m" << std::endl;
+            B.decrement(1);
+            std::cout << "New grade: " << B.getGrade() << std::endl;
+        }
+    }
+    catch (const std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    std::cout << std::endl;
+    std::cout << "Attempting to create Bureaucrats with invalid grades..." << std::endl;
+    try
+    {
+        Bureaucrat C("C", 151);
+    }
+    catch (const std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    std::cout << std::endl;
+    try
+    {
+        Bureaucrat D("D", 0);
+    }
+    catch (const std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    std::cout << std::endl;
+
+}
