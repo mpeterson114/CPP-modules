cccc1ea1a43f4b50a3226ef999d9f78632f1ca29
Megan Peterson <mlpeterson114@gmail.com>
2024-09-26 18:29:46 +0200

    06 started

---
+++ cpp06/ex00/inc/ScalarConverter.hpp
@@ -0,0 +1,13 @@
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+
+#include <string>
+#include <iostream>
+
+class ScalarConverter 
+{
+    static void convert(std::string const &input);
+
+};
+
+#endif
