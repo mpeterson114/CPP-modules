d1d2eeee4be2615089016128557bd31a7323ca79
Megan Peterson <mlpeterson114@gmail.com>
2024-09-30 11:28:24 +0200

    06

--- cpp06/ex00/inc/ScalarConverter.hpp
+++ cpp06/ex00/inc/ScalarConverter.hpp
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <iostream>
+#include <cmath>
 
 enum type 
 {
@@ -23,7 +24,29 @@ class ScalarConverter
         ScalarConverter& operator=(const ScalarConverter& copy);
         ~ScalarConverter();
 
-        static const std::string& getType();
+        static std::string getType(const std::string& input);
+        /* getType() format different from non-static getter functions:
+            1. Returns by value rather than 'const &' type: because function is static,
+            returning reference to local variable could cause dangling reference once
+            function goes out of scope
+                -doesn't have access to member variables throughout duration of program
+                via instantiation of object
+
+            2. Takes a parameter: doesn't have access to member variables through
+            object instantiation, relies on parameter for necessary info
+        */
+
+        static bool isInt(const std::string& input);
+        static bool isFloat(const std::string& input);
+        static bool isDouble(const std::string& input);
+        static void displayChar(const std::string& input);
+        static void displayInt(const std::string& input);
+        static void displayFloat(const std::string& input);
+        static void displayDouble(const std::string& input);
+        static void displayPseudoFloat(const std::string& input);
+        static void displayPseudoDouble(const std::string& input);
+
+        //Exception for unknown variable type??
 
 
     public:
