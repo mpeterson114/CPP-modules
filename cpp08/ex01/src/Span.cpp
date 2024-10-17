88faa7f102b9b188db071e94ac7b091984cfa958
Megan Peterson <mlpeterson114@gmail.com>
2024-10-17 17:16:29 +0200

    08/01 done

--- cpp08/ex01/src/Span.cpp
+++ cpp08/ex01/src/Span.cpp
@@ -4,27 +4,66 @@
 Span::Span(unsigned int N) : _N(N)
 {}
 
-Span::Span(const Span& other) : _N(other._N)
-{
-    
-}
+Span::Span(const Span& other) : _N(other._N), _vec(other._vec)
+{}
 
 Span& Span::operator=(const Span& other)
 {
-
+    if (this != &other)
+    {
+        this->_N = other._N;
+        this->_vec = other._vec;
+    }
+    return *this;
 }
 
-Span::~Span()
-{
-
-}
+Span::~Span() {}
 
 /* Member Functions */
 void Span::addNumber(int num)
 {
-
+    if (this->_vec.size() == this->_N)
+        throw std::runtime_error("Vector is already full");
+    this->_vec.push_back(num);
 }
 
-unsigned int shortestSpan();
-unsigned int longestSpan();
-void fillSpan(std::vector<int>::iterator begin, std::vector<int>::iterator end);
+unsigned int Span::longestSpan()
+{
+    if (this->_vec.size() <= 1)
+        throw std::runtime_error("No detectable span in vector");
+    
+    std::sort(this->_vec.begin(), this->_vec.end());
+    return this->_vec.back() - this->_vec.front();
+}
+
+unsigned int Span::shortestSpan()
+{
+    if (this->_vec.size() <= 1)
+        throw std::runtime_error("No detectable span in vector");
+
+    else if (this->_vec.size() == 2)
+        return this->longestSpan();
+    
+    std::sort(this->_vec.begin(), this->_vec.end());
+    size_t i = 1;
+    unsigned int shortest = this->_vec[i] - this->_vec[i - 1];
+
+    for (i = 2; i < this->_vec.size(); i++)
+    {
+        unsigned int diff = this->_vec[i] - this->_vec[i - 1];
+        if (diff < shortest)
+            shortest = diff;
+    }
+    return shortest;
+}
+
+void Span::fillSpan(std::vector<int>::iterator begin, std::vector<int>::iterator end)
+{
+    if ((unsigned int) std::distance(begin, end) >= (this->_N - this->_vec.size()))
+        throw std::runtime_error("Elements can't all fit in vector");
+    while (begin != end && this->_vec.size() < _N)
+    {
+        this->_vec.push_back(*begin);
+        ++begin;
+    }
+}
---
+++ cpp08/ex01/src/main.cpp
@@ -0,0 +1,125 @@
+#include "Span.hpp"
+#include <vector>
+
+int main()
+{
+    std::cout << "------------------------------" << std::endl;
+    std::cout << "-------- FROM SUBJECT --------" << std::endl;
+    std::cout << "------------------------------" << std::endl;
+    Span sp = Span(5);
+    sp.addNumber(9);
+    sp.addNumber(3);
+    sp.addNumber(17);
+    sp.addNumber(6);
+    sp.addNumber(11);
+    std::cout << sp.shortestSpan() << std::endl;
+    std::cout << sp.longestSpan() << std::endl;
+
+    std::cout << "---------------------------------------------" << std::endl;
+    std::cout << "-------- EMPTY & 1-NUMBER SPAN TESTS --------" << std::endl;
+    std::cout << "---------------------------------------------" << std::endl;
+    std::cout << "  -> Empty Span Created <-  " << std::endl;
+    Span empty(0);
+    try
+    {
+        std::cout << "Attempting to add number..." << std::endl;
+        empty.addNumber(1);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "\033[35m" << e.what() << "\033[0m" << std::endl;
+    }
+    std::cout << std::endl;
+    std::cout << "  -> Creating Span of 1 number <-  " << std::endl;
+    Span small(1);
+    std::cout << "Adding number <8>..." << std::endl;
+    small.addNumber(-42);
+    std::cout << "Adding another number <9>......" << std::endl;
+    try
+    {
+        small.addNumber(9);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "\033[35m" << e.what() << "\033[0m" << std::endl;
+    }
+    std::cout << "Shortest Span: " << std::endl;
+    try
+    {
+        small.shortestSpan();
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "\033[35m" << e.what() << "\033[0m" << std::endl;
+    }
+    std::cout << "Longest Span:" << std::endl;
+    try
+    {
+        small.longestSpan();
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "\033[35m" << e.what() << "\033[0m" << std::endl;
+    }
+    std::cout << std::endl;
+    std::cout << "----------------------------" << std::endl;
+    std::cout << "-------- MORE TESTS --------" << std::endl;
+    std::cout << "----------------------------" << std::endl;
+    Span medium(5);
+    std::cout << "Creating span 'medium' with max of 5 numbers <5, 20, 15, 10>" << std::endl;
+    medium.addNumber(5);
+    medium.addNumber(20);
+    medium.addNumber(15);
+    medium.addNumber(10);
+    std::cout << "Shortest span: " << medium.shortestSpan() << std::endl;
+    std::cout << "Longest span: " << medium.longestSpan() << std::endl;
+    std::cout << std::endl;
+    std::cout << "Adding <-50> to 'medium'...." << std::endl;
+    medium.addNumber(-50);
+    std::cout << "Shortest span: " << medium.shortestSpan() << std::endl;
+    std::cout << "Longest span: " << medium.longestSpan() << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "--------------------------------------" << std::endl;
+    std::cout << "-------- FILL LARGE SPAN TEST --------" << std::endl;
+    std::cout << "--------------------------------------" << std::endl;
+    std::cout << "Creating Span 'large' with 10000 numbers" << std::endl;
+    Span large(10000);
+    std::vector<int> temp;
+    for (int i = 1; i < 10000; i++)
+        temp.push_back(i * 5);
+    try
+    {
+        large.fillSpan(temp.begin(), temp.end());
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "\033[35m" << e.what() << "\033[0m" << std::endl;
+    }
+    try
+    {
+        std::cout << "Shortest span: " << large.shortestSpan() << std::endl;
+        std::cout << "Longest span: " << large.longestSpan() << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "\033[35m" << e.what() << "\033[0m" << std::endl;
+    }
+    std::cout << std::endl;
+    try
+    {
+        std::cout << "Adding <50001> to 'large'..." << std::endl;
+        large.addNumber(50001);
+        std::cout << "Shortest span: " << large.shortestSpan() << std::endl;
+        std::cout << "Longest span: " << large.longestSpan() << std::endl;
+        std::cout << std::endl;
+        std::cout << "Adding <50002> to 'large'..." << std::endl;
+        large.addNumber(50002);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "\033[35m" << e.what() << "\033[0m" << std::endl;
+    }
+    std::cout << std::endl;
+    return 0;
+}
