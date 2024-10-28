99abcc93138b01ad06743a474a326479fbb71331
Megan Peterson <mlpeterson114@gmail.com>
2024-10-28 18:46:10 +0100

    09 start

---
+++ cpp09/ex00/inc/BitcoinExchange.hpp
@@ -0,0 +1,24 @@
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+#include <iostream>
+#include <map>
+
+class BitcoinExchange 
+{
+    private:
+        std::map<std::string, double> _map;
+    
+    public:
+        BitcoinExchange();
+        BitcoinExchange(const BitcoinExchange& other);
+        BitcoinExchange& operator=(const BitcoinExchange& other);
+        ~BitcoinExchange();
+
+
+
+}
+
+
+
+#endif
